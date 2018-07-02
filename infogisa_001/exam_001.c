ae066bddf6e330aa7889a2d0abe750da7025fa2b
Unho Chang <unho.chang@gmail.com>
2018-07-02 10:47:48 +0900

    java code repo

---
+++ infogisa_001/exam_001.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+main()
+{
+	int i, j, k;
+	scanf("%d %d", &i, &j);
+	k = i + j;
+	printf("%d\n", k);
+}
---
+++ java_001/src/java_001/java_ch1_001.java
@@ -0,0 +1,15 @@
+package java_001;
+
+public class java_ch1_001 {
+
+	public static void main(String[] args) {
+		int i , j = 0;
+		for (i = 0; i < 8; i++)
+		{
+			j = j + i;
+		}
+		System.out.printf ("%d, %d\n", i, j);
+
+	}
+
+}
